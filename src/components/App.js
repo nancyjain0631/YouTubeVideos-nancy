2a212b056de4283b613b0bf6736daaafee57d113
Nancy Jain <nancyjain0631@gmail.com>
2021-01-12 22:28:27 +0530

    3

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import Youtube from '../APIs/Youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 const KEY='AIzaSyAuWKnJQYYwzE5ZP-5Wdp6OpNCOs-NOyg0';
 
@@ -22,12 +23,13 @@ class App extends React.Component{
     };
 
     onVideoSelect = (video) =>{
-        console.log('From the App!', video);
+        this.setState({selectedVideo: video});
     };
     render(){
         return (
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit}/>
+            <VideoDetail video={this.state.selectedVideo}/>
             <VideoList onVideoSelect= {this.onVideoSelect} videos={this.state.videos}/>
             </div>
         );
