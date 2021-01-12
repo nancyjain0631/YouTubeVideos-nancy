ade13d3591ad27a9de969b7b76f42b50a56da0ed
Nancy Jain <nancyjain0631@gmail.com>
2021-01-12 20:09:59 +0530

    2

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,8 +3,13 @@ import React from 'react';
 const VideoItem= ({video}) =>{
     return (
         <div className="item">
-            <img src={video.snippet.thumbnails.medium.url}/>
-            {video.snippet.title}
+            <img class="ui image" src={video.snippet.thumbnails.medium.url}/>
+            <div className="content">
+                <div className="header">
+                {video.snippet.title}
+                </div>
+            </div>
+            
         </div>
     );
 }
